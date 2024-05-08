e5cc2ca5483f964a71ce355ef8618348066e5cab
Julia-Gruzd <167764480+Julia-Gruzd@users.noreply.github.com>
2024-05-08 11:03:24 +0300

    navi

--- src/navigation.js
+++ src/navigation.js
@@ -3,117 +3,102 @@ import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
 export const headerData = {
   links: [
     {
-      text: 'Homes',
+      text: 'Портфолио',
       links: [
         {
-          text: 'SaaS',
+          text: 'Обычные кейсы',
           href: getPermalink('/homes/saas'),
         },
         {
-          text: 'Startup',
+          text: 'Микрокейсы',
           href: getPermalink('/homes/startup'),
-        },
-        {
-          text: 'Mobile App',
-          href: getPermalink('/homes/mobile-app'),
-        },
-        {
-          text: 'Personal',
-          href: getPermalink('/homes/personal'),
-        },
+        },       
       ],
     },
+
     {
-      text: 'Pages',
+      text: 'Услуги',
+      href: '#',
+    },
+
+    {
+      text: 'Отрасли',
+      href: '#',
+    },
+
+    {
+      text: 'Экспертиза',
       links: [
         {
-          text: 'Features (Anchor Link)',
+          text: 'Передача макетов в разработку',
           href: getPermalink('/#features'),
         },
         {
-          text: 'Services',
+          text: '100 шпаргалок по дизайну интерфейсов',
           href: getPermalink('/services'),
         },
         {
-          text: 'Pricing',
+          text: 'Статьи',
           href: getPermalink('/pricing'),
         },
         {
-          text: 'About us',
+          text: 'Книги',
           href: getPermalink('/about'),
         },
         {
-          text: 'Contact',
+          text: 'Email-рассылка',
           href: getPermalink('/contact'),
         },
-        {
-          text: 'Terms',
-          href: getPermalink('/terms'),
-        },
-        {
-          text: 'Privacy policy',
-          href: getPermalink('/privacy'),
-        },
-      ],
+       ],
     },
     {
-      text: 'Landing',
-      links: [
-        {
-          text: 'Lead Generation',
-          href: getPermalink('/landing/lead-generation'),
-        },
-        {
-          text: 'Long-form Sales',
-          href: getPermalink('/landing/sales'),
-        },
-        {
-          text: 'Click-Through',
-          href: getPermalink('/landing/click-through'),
-        },
-        {
-          text: 'Product Details (or Services)',
-          href: getPermalink('/landing/product'),
-        },
-        {
-          text: 'Coming Soon or Pre-Launch',
-          href: getPermalink('/landing/pre-launch'),
-        },
-        {
-          text: 'Subscription',
-          href: getPermalink('/landing/subscription'),
-        },
-      ],
-    },
-    {
-      text: 'Blog',
-      links: [
-        {
-          text: 'Blog List',
-          href: getBlogPermalink(),
-        },
-        {
-          text: 'Article',
-          href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
-        },
-        {
-          text: 'Article (with MDX)',
-          href: getPermalink('markdown-elements-demo-post', 'post'),
-        },
-        {
-          text: 'Category Page',
-          href: getPermalink('tutorials', 'category'),
-        },
-        {
-          text: 'Tag Page',
-          href: getPermalink('astro', 'tag'),
-        },
-      ],
-    },
-    {
-      text: 'Widgets',
+      text: 'О компании',
       href: '#',
     },
+    {
+      text: 'Контакты',
+      href: '#',
+    },
+    {
+      text: 'Работа у нас',
+      links: [
+        {
+          text: 'Вакансии',
+          href: getPermalink('/homes/saas'),
+        },
+        {
+          text: 'Выпускники',
+          href: getPermalink('/homes/startup'),
+        },       
+      ],
+    },
+    {
+      text: 'Соцсети',
+      links: [
+        {
+          text: 'Все соцсети',
+          href: getPermalink('/homes/saas'),
+        },
+        {
+          text: 'Telegram',
+          href: getPermalink('/homes/startup'),
+        }, 
+        {
+          text: 'ВКонтакте',
+          href: getPermalink('/homes/startup'),
+        }, 
+        {
+          text: 'VC',
+          href: getPermalink('/homes/startup'),
+        },     
+      ],
+    },
+    {
+      text: 'Дизайн-задачник',
+      href: '#',
+    },
+   
+   
   ],
   actions: [{ text: 'Download', href: 'https://github.com/onwidget/astrowind', target: '_blank' }],
 };
