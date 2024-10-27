b0e4ef8ecd4c1f4ea222e2d776841a532a3c4494
Olga Buberman <bub@sobakapav.ru>
2024-10-27 06:56:09 +0300

    Diff for cases

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,6 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import typographyPlugin from '@tailwindcss/typography';
+import daisyui from "daisyui";
 
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,json,md,mdx,svelte,ts,tsx,vue}'],
@@ -46,6 +47,6 @@ module.exports = {
       }
     },
   },
-  plugins: [typographyPlugin],
+  plugins: [typographyPlugin, daisyui],
   darkMode: 'class',
 };
