9471160e70560755a0e8d144840def31b23dcee4
Olga Buberman <bub@sobakapav.ru>
2026-02-18 15:12:57 +0300

    Portfolio

---
+++ src/pages/listPortfolioAsJson.json.js
@@ -0,0 +1,28 @@
+import Layout from '~/layouts/Layout.astro';
+import { portfolioFilters } from '~/navigation';
+
+import { getCollection } from 'astro:content';
+
+const filteredOut = ['aquaphor', 'broker', 'compel-password', 'documedd', 'easybuy', 'ecom', 'erna', 'faberlic-2', 'g-mini', 'just-info', 'jusfa', 'loglab', 'online-express', 'positive-tech', 'presentstar', 'simplefly', 'space307', 'torba', 'valer-ai', 'x5', 'x5-paket'];
+
+const services = portfolioFilters['Услуги'];
+const portfolio = await getCollection('portfolio');
+
+const portfolioItems = portfolio.filter(item => !filteredOut.includes(item.slug)).map(item => {
+  const itemImage = (item.data.thumbnail2 ? item.data.thumbnail2.src : item.data.thumbnail.src)?.substring('~/assets/images/portfolio/'.length);
+  
+  return {
+    name: item.slug,
+    year: item.data.publishYear,
+    title: item.data.title,
+    client: item.data.logo.alt,
+    services: item.data.tags.filter(tag => services[tag]),
+    image: itemImage ? `https://sobakapav.ru/images/portfolio/${itemImage}` : '',
+  }
+});
+
+export const GET = () => {
+  return new Response(JSON.stringify(portfolioItems), {
+    headers: { 'content-type': 'application/json' },
+  });
+};
